8b3b76cbe25185c97c5fe674dfa3e006c388af0f
Radu Cotescu <cotescu@adobe.com>
2021-03-01 17:36:51 +0100

    SLING-9999 - Remove cyclic dependency between scripting and servlets features
    * moved API to final location (bundle + API package)

--- src/test/java/org/apache/sling/scripting/spi/bundle/ResourceTypeTest.java
+++ src/test/java/org/apache/sling/scripting/spi/bundle/ResourceTypeTest.java
@@ -16,7 +16,7 @@
  ~ specific language governing permissions and limitations
  ~ under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-package org.apache.sling.servlets.resolver.bundle.tracker;
+package org.apache.sling.scripting.spi.bundle;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
